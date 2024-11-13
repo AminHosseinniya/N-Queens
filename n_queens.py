f8007423c6fb12dcf8a8251c759b76892d1e1656
MohammadAmin HosseinNiya <m.amin.hosseinniya@gmail.com>
2024-11-13 23:36:39 +0330

    Refactor: Make reachable squares finding modular.

--- n_queens.py
+++ n_queens.py
@@ -3,40 +3,45 @@ class Solution(object):
         """
         :type n: int
         :rtype: List[List[str]]
-        """
+        """ 
         placement_sets = []     # To hold all valid placements
         board = [[0] * n for _ in range(n)]     # To represent the chess board
-        board_border = range(len(board))
 
         ## The Placement Algorithm
-        square = [0, 0]     # Put first square
-        board[square[0]][square[1]] = 1     # Show the queen on the board
+        queen_0 = [0, 0]     # Put first square
+        board[queen_0[0]][queen_0[1]] = 1     # Show the queen on the board
 
         reachable_squares = list()
-        # Horizontal reachable squares are not examined, 'cause no queen is gonne be out in same row with queen in hand.
+        reachable_squares = self.reach_finder(board, queen_0, reachable_squares)
 
-        # Vertical reachable squares:
-        [reachable_squares.append([x, square[1]]) for x in board_border]                # Reachable squares toward down
-
-        # Diagonal reachable squares:
-        for step in range(len(board))[1:]:
-            if square[0]+step in board_border and square[1]-step in board_border:       # Reachable squares toward bottom-lef
-                reachable_squares.append([square[0]+step,square[1]-step])
-            if square[0]+step in board_border and square[1]+step in board_border:       # Reachable squares toward bottom-right
-                reachable_squares.append([square[0]+step,square[1]+step])
-        print("\nreachable cells: ", reachable_squares)
 
         # To show the board:
         for square in reachable_squares:
             board[square[0]][square[1]] = "*"
-
-
-
-
-
+        
+        print("\nreachable cells: ", reachable_squares)
         print("\nboard:")
+
         for row in board:
             print("  ".join(str(element) for element in row))
 
+
+    def reach_finder(self, board, queen_coords, reach_list):
+        board_border = range(len(board))
+        # Horizontal reachable squares are not examined, 'cause no queen is gonne be out in same row with queen in hand.
+        # Vertical reachable squares:
+        [reach_list.append([x, queen_coords[1]]) for x in board_border]        # Reachable squares toward down
+
+        # Diagonal reachable squares:
+        for step in range(len(board))[1:]:
+            if queen_coords[0]+step in board_border and queen_coords[1]-step in board_border:       # Reachable squares toward bottom-lef
+                reach_list.append([queen_coords[0]+step, queen_coords[1]-step])
+            if queen_coords[0]+step in board_border and queen_coords[1]+step in board_border:       # Reachable squares toward bottom-right
+                reach_list.append([queen_coords[0]+step, queen_coords[1]+step])
+
+        return reach_list
+
+
+
 solution = Solution()
-solution.solveNQueens(4)
+solution.solveNQueens(5)
