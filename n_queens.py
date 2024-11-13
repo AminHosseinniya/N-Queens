677fe3a7f4d472598e4d4b5b70fe4cecd1aee435
CaptainMAH <AminHosseinniya@gmail.com>
2024-11-13 13:50:33 +0330

    Feat: Put first queen on the board.

--- n_queens.py
+++ n_queens.py
@@ -6,6 +6,10 @@ class Solution(object):
         """
         placement_sets = []     # To hold all valid placements
         board = [[0] * n for _ in range(n)]     # To represent the chess board
+
+        ## The Placement Algorithm
+        square = [0, 0]     # Put first square
+        board[square[0]][square[1]] = 1     # Show the queen on the board
         
         for row in board:
             print(row)
