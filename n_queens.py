426756e8439ced58cd8a95641cc00386a80d6d32
CaptainMAH <AminHosseinniya@gmail.com>
2024-12-22 17:19:33 +0330

    Recursive algorithm is set. errors happen for n=7 and n=8.

--- n_queens.py
+++ n_queens.py
@@ -6,25 +6,64 @@ class Solution(object):
         """ 
         placements_set = list()     # To hold all valid placements
         board = [[0] * n for _ in range(n)]     # To represent the chess board
-        
-        valid_squares = [n for _ in range(n)]
-        queens = [[i, 0] for i in range(n)]
-        rows_guide = [{"status": False, "num_of_valids": 0} for _ in range(n)]      # "status" of each row should be True if there are still
+        squares = [[row, col] for row in range(n) for col in range(n)]
+        queens = [[None, None] for _ in range(n)]
+        valid_squares = [[None, None] for _ in range(n)]
+        status = [True]*n
+        # valid_squares = [n for _ in range(n)]
+        # rows_guide = [{"status": False, "num_of_valids": 0} for _ in range(n)]      # "status" of each row should be True if there are still                                              
                                                                                     # valid squares in that row that are not examined yet, 
                                                                                     # and "num_of_valids" show number of this valid sqaures.
-        print("queens: ", queens)
-        # queens[0][1] = 0
-        for i in range(n):
-            # reachable_squares = list()
-            queens[0][1] = 0   # Put queen of first row
-            print("queens after: ", queens)
+        # print("queens: ", queens)
 
-            # board[queens[0][0]][queens[0][1]] = 1     # Show the queen on the board
-            reachable_squares = self.reach_finder(board, queens)
+        
+        self.result = self.put_queen(0, queens, board, squares, valid_squares, placements_set, status)
+        print(self.result)
 
-        # print(valid_squares)            
+        # reachable_squares = self.reach_finder(board, queens)
+        # self.show_the_board(board, reachable_squares)
+        # print(self.placements_set)
+    
 
-        self.show_the_board( board, reachable_squares)
+
+    def put_queen(self, row, queens, board, squares, valid_squares, placements_set, status):
+        if row == -1:
+            print(row)
+            print(placements_set)
+            return placements_set
+
+        print("\n========================")
+        reachable_squares = self.reach_finder(board, queens)    # get valid square through the whole board
+        if status[row] == True:
+            valid_squares[row] = [square for square in squares if square[0]==row and square not in reachable_squares]   # get valid squares of considering row
+            status[row] = False
+        print(f"valid_squares: {valid_squares}")
+
+        if len(valid_squares[row]) != 0:
+            print("SAFA")
+            print(f"row: {row}")
+            queens[row] = valid_squares[row][0]     # out queen of considering row in first available position
+            valid_squares[row].pop(0)    # drop filled square from valid squares of considering row
+            print(f"valid_squares after pop: {valid_squares}")
+            print(f"queens: {queens}")
+            print(f"placements_set: {placements_set}")
+            if row == len(board)-1:
+                placements_set.append(queens)
+                print(f"placements_set after append: {placements_set}")
+                self.put_queen(row, queens[:], board, squares, valid_squares[:], placements_set, status[:])  # go for next row!
+            else:
+                status[row+1] = True
+                self.put_queen(row+1, queens[:], board, squares, valid_squares[:], placements_set, status[:])  # go for next row!
+        else:
+            print("NOT SAFA")
+            print(f"row: {row}")
+            queens[(row-1):] = [[None, None]]*len(queens[(row-1):])
+            status[row-1] = False    # not allowd to revalculate valid squares
+            print(f"queens before going back: {queens}")
+            self.put_queen(row-1, queens[:], board, squares, valid_squares[:], placements_set, status[:])  # go for previous row!
+        
+        
+        # return placements_set
 
 
 
@@ -34,6 +73,10 @@ class Solution(object):
         # Horizontal reachable squares are not examined, 'cause no queen is gonne be out in same row with queen in hand.
         
         for queen in queens:
+            # Ensure queen is already initiated:
+            if queen == [None, None]:
+                continue
+            
             # Vertical reachable squares:
             [reachable_squares.append([x, queen[1]]) for x in board_border]        # Reachable squares toward down
 
@@ -45,6 +88,7 @@ class Solution(object):
                     reachable_squares.append([queen[0]+step, queen[1]+step])
 
         return reachable_squares
+
 
 
     def show_the_board(self, board, reachable_squares):
@@ -59,4 +103,4 @@ class Solution(object):
 
 
 solution = Solution()
-solution.solveNQueens(4)
+solution.solveNQueens(6)
